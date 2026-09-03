39418d891dc15322d3a5f06e668b60aed38dbd5a
allenzhang61 <zhyoulun@gmail.com>
2026-09-04 07:12:09 +1000

    cpp

---
+++ ProgramLanguage/cpp-primer-plus/codes/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int a = 1;
+    // std::cin>>a;
+    std::cout<<"cout: "<<a<<"\n";
+    std::clog<<"clog: "<<a<<"\n";
+    std::cerr<<"cerr: "<<a<<"\n";
+    // std::cout<<"cout: "<<a<<std::endl;
+    // std::clog<<"clog: "<<a<<std::endl;
+    // std::cerr<<"cerr: "<<a<<std::endl;
+}
